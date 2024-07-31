893bf5f151f0c304bb75085914bd72cc2e1f0aa8
varshni-ramki <121806730+varshni-ramki@users.noreply.github.com>
2024-07-31 11:44:18 +0530

    Create PayPalPayment.java

---
+++ Week1_assignment/Design Patterns/ Implementing the Strategy Pattern/PayPalPayment.java
@@ -0,0 +1,12 @@
+public class PayPalPayment implements PaymentStrategy {
+    private String email;
+
+    public PayPalPayment(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Processing PayPal payment of $" + amount + " for email: " + email);
+    }
+}
