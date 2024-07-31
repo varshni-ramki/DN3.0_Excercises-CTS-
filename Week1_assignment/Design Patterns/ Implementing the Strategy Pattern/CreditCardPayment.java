15aad1d771e5aaf8202bac75696554294b4c92f0
varshni-ramki <121806730+varshni-ramki@users.noreply.github.com>
2024-07-31 11:41:10 +0530

    Create CreditCardPayment.java

---
+++ Week1_assignment/Design Patterns/ Implementing the Strategy Pattern/CreditCardPayment.java
@@ -0,0 +1,14 @@
+public class CreditCardPayment implements PaymentStrategy {
+    private String cardNumber;
+    private String cardHolderName;
+
+    public CreditCardPayment(String cardNumber, String cardHolderName) {
+        this.cardNumber = cardNumber;
+        this.cardHolderName = cardHolderName;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Processing credit card payment of $" + amount + " for card: " + cardNumber);
+    }
+}
