dffe72eebfc1b2771b7d4b07ddb470780f364063
varshni-ramki <121806730+varshni-ramki@users.noreply.github.com>
2024-07-31 08:29:29 +0530

    Create Order.java

---
+++ Week1_assignment/DSA/Sorting Customer Orders/Order.java
@@ -0,0 +1,25 @@
+public class Order {
+    private int orderId;
+    private String customerName;
+    private double totalPrice;
+
+    public Order(int orderId, String customerName, double totalPrice) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.totalPrice = totalPrice;
+    }
+
+    // Getters
+    public int getOrderId() { return orderId; }
+    public String getCustomerName() { return customerName; }
+    public double getTotalPrice() { return totalPrice; }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", customerName='" + customerName + '\'' +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
+}
