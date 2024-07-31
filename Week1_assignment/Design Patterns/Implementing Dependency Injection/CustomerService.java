edb0146ca512de392a18c3ab493ff9eefd0516de
varshni-ramki <121806730+varshni-ramki@users.noreply.github.com>
2024-07-31 11:52:52 +0530

    Create CustomerService.java

---
+++ Week1_assignment/Design Patterns/Implementing Dependency Injection/CustomerService.java
@@ -0,0 +1,12 @@
+  public class CustomerService {
+    private CustomerRepository customerRepository;
+
+    // Constructor injection
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer getCustomer(String id) {
+        return customerRepository.findCustomerById(id);
+    }
+}
