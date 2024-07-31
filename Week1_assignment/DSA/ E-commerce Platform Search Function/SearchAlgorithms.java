db6e6868f7c2a116a1536f8f434994d6697eda30
varshni-ramki <121806730+varshni-ramki@users.noreply.github.com>
2024-07-31 08:22:56 +0530

    Create SearchAlgorithms.java

---
+++ Week1_assignment/DSA/ E-commerce Platform Search Function/SearchAlgorithms.java
@@ -0,0 +1,12 @@
+public class SearchAlgorithms {
+
+    // Linear Search
+    public static Product linearSearch(Product[] products, int productId) {
+        for (Product product : products) {
+            if (product.getProductId() == productId) {
+                return product;
+            }
+        }
+        return null; // Not found
+    }
+}
